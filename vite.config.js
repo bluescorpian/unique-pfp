12d90ce43f451fb4841d34264862b27389d5c18b
bluescorpian519 <dsharryh27@gmail.com>
2023-03-26 00:58:37 +0200

    vite stuff bruh

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command, mode }) => {
+  const baseConfig = {};
+
   if (mode === 'gh-pages') {
     return {
+      ...baseConfig,
       base: 'unique-pfp',
     };
-  } else return {};
+  } else return baseConfig;
 });
